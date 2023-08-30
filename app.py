b38e5f5aad7619f5258419ff61369b5e48d5643a
jiaqiangbandongg <396370321@qq.com>
2023-08-30 20:39:56 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,17 +5,19 @@ st.write("# 凑单计算器")
 
 st.write("在输入框输入多个商品价格，用`逗号`或者`空格`隔开隔开，然后点击`计算`按钮，即可得到最佳凑单商品的价格")
 
+target = st.text_input("输入你要凑的目标 默认是600",value=600)
+target = float(target)
 prices_text = st.text_area("商品价格 例如: 15.8,57,38.5,22.8,65,355,19,28,58,88,63.1")
 
 click = st.button("计算", type="primary")
 
 
-def fun(prices):
+def fun(prices,target):
     solver = pywraplp.Solver.CreateSolver("SCIP")
     x = {}
     for i in range(len(prices)):
         x[i] = solver.IntVar(0, 1, "x[%i]" % i)
-    solver.Add(sum(x[i]*price for i,price in enumerate(prices)) >= 600)
+    solver.Add(sum(x[i]*price for i,price in enumerate(prices)) >= target)
     solver.Minimize(solver.Sum([x[i]*price for i,price in enumerate(prices)]))
     solver.Solve()
     s = 0
@@ -29,6 +31,6 @@ def fun(prices):
 if click:
     prices = re.findall("\d+\.?\d+",prices_text)
     prices = list(map(float,prices))
-    s,ret = fun(prices)
+    s,ret = fun(prices,target)
     st.write(f'最优价格总和:{s}')
     st.write(f'选择这些商品:{ret}')
